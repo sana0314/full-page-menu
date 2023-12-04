579c71c6db56b8f4146c65e3192ea6ed3037dd73
Sana Fatima <57403594+sana0314@users.noreply.github.com>
2023-12-04 11:28:24 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,411 @@
+console.log("hello");
+
+
+
+
+
+    function changeStyle(){
+      let one = document.getElementById("one");
+      one.style.opacity = "0.9";
+      one.style.transition= "all 1s ease-in-out";
+
+      let two = document.getElementById("two");
+      two.style.opacity = "0.2";
+      
+
+      let three = document.getElementById("three");
+      three.style.opacity = "0.2";
+
+      let four = document.getElementById("four");
+      four.style.opacity = "0.2";
+
+      let five = document.getElementById("five");
+      five.style.opacity = "0.2";
+
+  }
+
+  function changeThis(){
+    let one = document.getElementById("one");
+    one.style.opacity = "0.9";
+
+    let two = document.getElementById("two");
+    two.style.opacity = "0.9";
+
+    let three = document.getElementById("three");
+    three.style.opacity = "0.9";
+
+    let four = document.getElementById("four");
+    four.style.opacity = "0.9";
+
+    let five = document.getElementById("five");
+    five.style.opacity = "0.9";
+
+}
+
+// -----------------------------------------------------------
+function changeStyle2(){
+  let one = document.getElementById("two");
+  one.style.opacity = "0.9";
+  one.style.transition= "all 1s ease-in-out";
+  
+
+  let two = document.getElementById("one");
+  two.style.opacity = "0.2";
+
+  let three = document.getElementById("three");
+  three.style.opacity = "0.2";
+
+  let four = document.getElementById("four");
+  four.style.opacity = "0.2";
+
+  let five = document.getElementById("five");
+  five.style.opacity = "0.2";
+
+}
+
+function changeThis2(){
+let one = document.getElementById("two");
+one.style.opacity = "0.9";
+
+let two = document.getElementById("one");
+two.style.opacity = "0.9";
+
+let three = document.getElementById("three");
+three.style.opacity = "0.9";
+
+let four = document.getElementById("four");
+four.style.opacity = "0.9";
+
+let five = document.getElementById("five");
+five.style.opacity = "0.9";
+
+}
+
+// -----------------------------------------------------------
+
+function changeStyle3(){
+  let one = document.getElementById("three");
+  one.style.opacity = "0.9";
+  one.style.transition= "all 1s ease-in-out";
+
+  let two = document.getElementById("one");
+  two.style.opacity = "0.2";
+
+  let three = document.getElementById("two");
+  three.style.opacity = "0.2";
+
+  let four = document.getElementById("four");
+  four.style.opacity = "0.2";
+
+  let five = document.getElementById("five");
+  five.style.opacity = "0.2";
+
+}
+
+function changeThis3(){
+let one = document.getElementById("three");
+one.style.opacity = "0.9";
+
+let two = document.getElementById("one");
+two.style.opacity = "0.9";
+
+let three = document.getElementById("two");
+three.style.opacity = "0.9";
+
+let four = document.getElementById("four");
+four.style.opacity = "0.9";
+
+let five = document.getElementById("five");
+five.style.opacity = "0.9";
+
+}
+
+// -----------------------------------------------------------
+function changeStyle4(){
+  let one = document.getElementById("four");
+  one.style.opacity = "0.9";
+  one.style.transition= "all 1s ease-in-out";
+
+  let two = document.getElementById("one");
+  two.style.opacity = "0.2";
+
+  let three = document.getElementById("two");
+  three.style.opacity = "0.2";
+
+  let four = document.getElementById("three");
+  four.style.opacity = "0.2";
+
+  let five = document.getElementById("five");
+  five.style.opacity = "0.2";
+
+}
+
+function changeThis4(){
+let one = document.getElementById("four");
+one.style.opacity = "0.9";
+
+let two = document.getElementById("one");
+two.style.opacity = "0.9";
+
+let three = document.getElementById("two");
+three.style.opacity = "0.9";
+
+let four = document.getElementById("one");
+four.style.opacity = "0.9";
+
+let five = document.getElementById("five");
+five.style.opacity = "0.9";
+
+}
+
+
+// -----------------------------------------------------------
+function changeStyle5(){
+  let one = document.getElementById("five");
+  one.style.opacity = "0.9";
+  one.style.transition= "all 1s ease-in-out";
+
+  let two = document.getElementById("one");
+  two.style.opacity = "0.2";
+
+  let three = document.getElementById("two");
+  three.style.opacity = "0.2";
+
+  let four = document.getElementById("three");
+  four.style.opacity = "0.2";
+
+  let five = document.getElementById("four");
+  five.style.opacity = "0.2";
+
+}
+
+function changeThis5(){
+let one = document.getElementById("five");
+one.style.opacity = "0.9";
+
+let two = document.getElementById("one");
+two.style.opacity = "0.9";
+
+let three = document.getElementById("two");
+three.style.opacity = "0.9";
+
+let four = document.getElementById("four");
+four.style.opacity = "0.9";
+
+let five = document.getElementById("three");
+five.style.opacity = "0.9";
+
+}
+
+
+
+console.log("hello");
+
+//*******************----------------------------------------------------**************************//
+
+
+
+
+function changeStyle(){
+    let one = document.getElementById("one");
+    one.style.opacity = "0.9";
+    one.style.transition= "all 1s ease-in-out";
+
+    let two = document.getElementById("two");
+    two.style.opacity = "0.2";
+    
+
+    let three = document.getElementById("three");
+    three.style.opacity = "0.2";
+
+    let four = document.getElementById("four");
+    four.style.opacity = "0.2";
+
+    let five = document.getElementById("five");
+    five.style.opacity = "0.2";
+
+}
+
+function changeThis(){
+  let one = document.getElementById("one");
+  one.style.opacity = "0.9";
+
+  let two = document.getElementById("two");
+  two.style.opacity = "0.9";
+
+  let three = document.getElementById("three");
+  three.style.opacity = "0.9";
+
+  let four = document.getElementById("four");
+  four.style.opacity = "0.9";
+
+  let five = document.getElementById("five");
+  five.style.opacity = "0.9";
+
+}
+
+// -----------------------------------------------------------
+function changeStyle2(){
+let one = document.getElementById("two");
+one.style.opacity = "0.9";
+one.style.transition= "all 1s ease-in-out";
+
+
+let two = document.getElementById("one");
+two.style.opacity = "0.2";
+
+let three = document.getElementById("three");
+three.style.opacity = "0.2";
+
+let four = document.getElementById("four");
+four.style.opacity = "0.2";
+
+let five = document.getElementById("five");
+five.style.opacity = "0.2";
+
+}
+
+function changeThis2(){
+let one = document.getElementById("two");
+one.style.opacity = "0.9";
+
+let two = document.getElementById("one");
+two.style.opacity = "0.9";
+
+let three = document.getElementById("three");
+three.style.opacity = "0.9";
+
+let four = document.getElementById("four");
+four.style.opacity = "0.9";
+
+let five = document.getElementById("five");
+five.style.opacity = "0.9";
+
+}
+
+// -----------------------------------------------------------
+
+function changeStyle3(){
+let one = document.getElementById("three");
+one.style.opacity = "0.9";
+one.style.transition= "all 1s ease-in-out";
+
+let two = document.getElementById("one");
+two.style.opacity = "0.2";
+
+let three = document.getElementById("two");
+three.style.opacity = "0.2";
+
+let four = document.getElementById("four");
+four.style.opacity = "0.2";
+
+let five = document.getElementById("five");
+five.style.opacity = "0.2";
+
+}
+
+function changeThis3(){
+let one = document.getElementById("three");
+one.style.opacity = "0.9";
+
+let two = document.getElementById("one");
+two.style.opacity = "0.9";
+
+let three = document.getElementById("two");
+three.style.opacity = "0.9";
+
+let four = document.getElementById("four");
+four.style.opacity = "0.9";
+
+let five = document.getElementById("five");
+five.style.opacity = "0.9";
+
+}
+
+// -----------------------------------------------------------
+function changeStyle4(){
+let one = document.getElementById("four");
+one.style.opacity = "0.9";
+one.style.transition= "all 1s ease-in-out";
+
+let two = document.getElementById("one");
+two.style.opacity = "0.2";
+
+let three = document.getElementById("two");
+three.style.opacity = "0.2";
+
+let four = document.getElementById("three");
+four.style.opacity = "0.2";
+
+let five = document.getElementById("five");
+five.style.opacity = "0.2";
+
+}
+
+function changeThis4(){
+let one = document.getElementById("four");
+one.style.opacity = "0.9";
+
+let two = document.getElementById("one");
+two.style.opacity = "0.9";
+
+let three = document.getElementById("two");
+three.style.opacity = "0.9";
+
+let four = document.getElementById("one");
+four.style.opacity = "0.9";
+
+let five = document.getElementById("five");
+five.style.opacity = "0.9";
+
+}
+
+
+// -----------------------------------------------------------
+function changeStyle5(){
+let one = document.getElementById("five");
+one.style.opacity = "0.9";
+one.style.transition= "all 1s ease-in-out";
+
+let two = document.getElementById("one");
+two.style.opacity = "0.2";
+
+let three = document.getElementById("two");
+three.style.opacity = "0.2";
+
+let four = document.getElementById("three");
+four.style.opacity = "0.2";
+
+let five = document.getElementById("four");
+five.style.opacity = "0.2";
+
+}
+
+function changeThis5(){
+let one = document.getElementById("five");
+one.style.opacity = "0.9";
+
+let two = document.getElementById("one");
+two.style.opacity = "0.9";
+
+let three = document.getElementById("two");
+three.style.opacity = "0.9";
+
+let four = document.getElementById("four");
+four.style.opacity = "0.9";
+
+let five = document.getElementById("three");
+five.style.opacity = "0.9";
+
+}
+
+
+
+
+
+
+
+
+
+
+
